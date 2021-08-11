b2997acd6f3d4c8feb61f1360a96515ccd5514ac
yev <josephharveyangeles@gmail.com>
2021-08-11 16:27:12 +0800

    v4.7.0     - added LocalDate serializers     - updated versioning     - {vertx.major.minor}

---
+++ src/main/java/ph/com/nightowlstudios/entity/serializer/LocalDateDeserializer.java
@@ -0,0 +1,33 @@
+package ph.com.nightowlstudios.entity.serializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+/**
+ * @author <a href="mailto:josephharveyangeles@gmail.com">Joseph Harvey Angeles - <i>yev</i></a>
+ * @since 8/11/21
+ **/
+public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+
+    protected LocalDateDeserializer() {
+        this(LocalDate.class);
+    }
+
+    protected LocalDateDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        try {
+            long epoch = Long.parseLong(p.getText());
+            return LocalDate.ofEpochDay(epoch);
+        } catch (NumberFormatException e) {
+            return LocalDate.parse(p.getText());
+        }
+    }
+}
---
+++ src/main/java/ph/com/nightowlstudios/entity/serializer/LocalDateSerializer.java
@@ -0,0 +1,29 @@
+package ph.com.nightowlstudios.entity.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+/**
+ * @author <a href="mailto:josephharveyangeles@gmail.com">Joseph Harvey Angeles - <i>yev</i></a>
+ * @since 8/11/21
+ **/
+public class LocalDateSerializer extends StdSerializer<LocalDate> {
+
+    protected LocalDateSerializer() {
+        this(LocalDate.class);
+    }
+
+    protected LocalDateSerializer(Class<LocalDate> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        long epoch = value.toEpochDay();
+        gen.writeString(Long.toString(epoch));
+    }
+}
